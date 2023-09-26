1a0c8ad8a543b21ab3bdca457dfd212e782195c6
blackbody256 <145652020+blackbody256@users.noreply.github.com>
2023-09-26 21:19:16 +0300

    Update1 main.c
    I have changed it so that the area of the plot is the one requested.
    I have changed even its data type to doubles because the value of the area of the plot have decimal points

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main(void){
 int location;
-int length_decimals;
+double Area_decimals;
 int price;
 printf("What is the region location of the plot\?\n1.Central Uganda\n2.Northern Uganda\n3.Eastern Uganda\n4.Western Uganda\n");
 printf("NOTE: The answer should be 1,2,3 OR 4 only\n");
 scanf("%d",&location);
-printf("Enter the boundary length of the plot in decimals\n");
-scanf("%i", &length_decimals);
+printf("Enter the Area of the plot in decimals\n");
+scanf("%.3lf", &Area_decimals);
 if (location==1){
-    price= length_decimals*2000000;
+    price= Area_decimals*2000000;
     printf("The price of the land is %i\n", price);
 }else if (location==2){
-price= length_decimals*1000000;
+price= Area_decimals*1000000;
 printf("The price of the land is %i\n", price);
 }else if (location==3){
-price= length_decimals*1500000;
+price= Area_decimals*1500000;
 printf("The price of the land is %i\n", price);
 }else if (location==4){
-price= length_decimals*1200000;
+price= Area_decimals*1200000;
 printf("The price of the land is %i\n", price);
 }else{
 printf("Have some sense and read \"1 to 4\"\n");
