6f52ba59f322e64cdf7e32037cc392795db69c79
blackbody256 <sseluyindaeva@gmail.com>
2023-10-17 16:03:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 int main(void){
 int location;
-double Area_decimals;
-int price;
+unsigned long long int Area_meters;//since there are no negative Areas
+unsigned long long int Area_decimals;//because i need a large of area and prices
+unsigned long long int price;
 printf("What is the region location of the plot\?\n1.Central Uganda\n2.Northern Uganda\n3.Eastern Uganda\n4.Western Uganda\n");
 printf("NOTE: The answer should be 1,2,3 OR 4 only\n");
 scanf("%d",&location);
-printf("Enter the Area of the plot in decimals\n");
-scanf("%.3lf", &Area_decimals);
+printf("Enter the boundary Area of the plot in Meters\n");
+scanf("%i", &Area_meters);
+Area_decimals = Area_meters*0.024;
 if (location==1){
-    price= Area_decimals*2000000;
-    printf("The price of the land is %i\n", price);
+    price= Area_decimals*150000;
+    printf("The price of the land is %i Uganda shillings\n", price);
 }else if (location==2){
-price= Area_decimals*1000000;
-printf("The price of the land is %i\n", price);
+price= Area_decimals*100000;
+printf("The price of the land is %i Uganda shillings\n", price);
 }else if (location==3){
-price= Area_decimals*1500000;
-printf("The price of the land is %i\n", price);
+price= Area_decimals*1300000;
+printf("The price of the land is %i Uganda shillings\n", price);
 }else if (location==4){
-price= Area_decimals*1200000;
-printf("The price of the land is %i\n", price);
+price= Area_decimals*120000;
+printf("The price of the land is %i Uganda shillings\n", price);
 }else{
 printf("Have some sense and read \"1 to 4\"\n");
 }
